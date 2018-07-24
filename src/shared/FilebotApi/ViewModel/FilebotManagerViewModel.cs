13f6b7f664de2092f6e5bb7886afff64e3438d9d
mk <mattkramers@gmail.com>
2018-07-23 22:08:46 -0400

    Code cleanup

--- src/shared/FilebotApi/ViewModel/FilebotManagerViewModel.cs
+++ src/shared/FilebotApi/ViewModel/FilebotManagerViewModel.cs
@@ -1,19 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using FilebotApi.Result;
+using System.Windows.Input;
 using GalaSoft.MvvmLight;
-using MVVM;
 using MVVM.ViewModel;
 
 namespace FilebotApi.ViewModel
 {
     public class FilebotManagerViewModel : ViewModelBase
     {
+        public Filebot Filebot { get; }
+        public LocalCleanerViewModel CompletedDirectory { get; }
+        public DirectoryViewModel[] Directories { get; }
+
         public FilebotManagerViewModel()
         {
             const string input = @"D:\Unsorted\completed";
@@ -21,14 +20,12 @@ namespace FilebotApi.ViewModel
             const string tvDir = @"F:\videos\TV Shows";
             const string moviesDir = @"F:\videos\Movies";
 
-            var appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
 
-            var directory = Path.Combine(appData, "SyncWatcher", "Settings");
+            string directory = Path.Combine(appData, "SyncWatcher", "Settings");
 
             if (!Directory.Exists(directory))
-            {
                 Directory.CreateDirectory(directory);
-            }
 
             string settingsPath = Path.Combine(directory, "settings.xml");
             string recordsPath = Path.Combine(directory, "amclog.txt");
@@ -36,10 +33,10 @@ namespace FilebotApi.ViewModel
             Directories = new[]
             {
                 new DirectoryViewModel(tvDir, "TV"),
-                new DirectoryViewModel(moviesDir, "MOVIES"),
+                new DirectoryViewModel(moviesDir, "MOVIES")
             };
 
-            if (Filebot.TryCreate(settingsPath, recordsPath, out var filebot))
+            if (Filebot.TryCreate(settingsPath, recordsPath, out Filebot filebot))
             {
                 filebot.Stopped += Filebot_Completed;
 
@@ -61,27 +58,21 @@ namespace FilebotApi.ViewModel
         {
             Application.Current.Dispatcher.Invoke(() =>
             {
-                var refreshCommand = CompletedDirectory.DirectoryViewModel.RefreshCommand;
+                ICommand refreshCommand = CompletedDirectory.DirectoryViewModel.RefreshCommand;
                 if (refreshCommand.CanExecute(null))
-                {
                     refreshCommand.Execute(null);
-                }
             });
         }
 
         public event EventHandler FilebotStarted
         {
-            add { Filebot.Started += value; }
-            remove { Filebot.Started -= value; }
+            add => Filebot.Started += value;
+            remove => Filebot.Started -= value;
         }
         public event EventHandler<Filebot.FileBotOrganizeEventArgs> FilebotCompleted
         {
-            add { Filebot.Stopped += value; }
-            remove { Filebot.Stopped -= value; }
+            add => Filebot.Stopped += value;
+            remove => Filebot.Stopped -= value;
         }
-
-        public Filebot Filebot { get; }
-        public LocalCleanerViewModel CompletedDirectory { get; }
-        public DirectoryViewModel[] Directories { get; }
     }
 }
