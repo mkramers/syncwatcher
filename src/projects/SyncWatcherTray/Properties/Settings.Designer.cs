e4db6194a071a0cd5ae18a961f75deb574914f1e
mk <mattkramers@gmail.com>
2018-09-28 12:48:19 -0400

    Update default path

--- src/projects/SyncWatcherTray/Properties/Settings.Designer.cs
+++ src/projects/SyncWatcherTray/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace SyncWatcherTray.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -61,7 +61,7 @@ namespace SyncWatcherTray.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("D:\\Unsorted\\completed")]
+        [global::System.Configuration.DefaultSettingValueAttribute("D:\\sync")]
         public string CompletedDirectory {
             get {
                 return ((string)(this["CompletedDirectory"]));
