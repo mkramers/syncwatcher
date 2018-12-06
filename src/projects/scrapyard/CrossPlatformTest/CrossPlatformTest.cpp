cde61da8ff6fbcdc85b42664676fd9e353f60220
mk <mattkramers@gmail.com>
2018-12-05 23:49:55 -0500

    Add cmake test project

---
+++ src/projects/scrapyard/CrossPlatformTest/CrossPlatformTest.cpp
@@ -0,0 +1,12 @@
+﻿// CrossPlatformTest.cpp : Defines the entry point for the application.
+//
+
+#include "CrossPlatformTest.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
